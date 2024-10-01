3b68daba7376909fcd8e0a5a7096666c83edaee2
Mateus Nardo <m.nardo@epilot.cloud>
2024-10-01 17:15:12 +0200

    chore: update example with variables

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,6 +7,19 @@ terraform {
   }
 }
 
+variable "epilot_auth" {
+  type = string
+}
+variable "template_variables_api_url" {
+  type    = string
+  default = "https://template-variables-api.dev.sls.epilot.io"
+}
+
 provider "epilot-variable" {
-  # Configuration options
+  epilot_auth = var.epilot_auth
+  server_url = var.template_variables_api_url
+}
+
+resource "epilot-variable_custom_variable" "my_variable" {
+  # (resource arguments)
 }
